a73cee0ed36521fa924279e2dce5406dd86064c8
Wellington <w_santos1993@outlook.com>
2020-08-13 15:22:39 -0300

    Tipos de dados e seus valores

---
+++ tiposDeDados.js
@@ -0,0 +1,72 @@
+//Valores...
+
+// String usando " dupla.
+let nome = "Wellington";
+
+console.log(nome);
+
+// String usando ' simples.
+
+let sobrenome =  'Santos';
+
+console.log(sobrenome);
+
+// Tipo inteiro.
+
+let idade = 27;
+
+console.log(idade);
+
+// Número decimal.
+
+let altura = 1.77;
+
+console.log(altura);
+
+// Boleano true.
+let diaEstaChuvoso = true;
+
+console.log(diaEstaChuvoso);
+
+// Boleano false.
+
+let vouParaPraia = false;
+
+console.log(vouParaPraia);
+
+// not a number.
+
+let naoNumerico = NaN;
+
+console.log(naoNumerico);
+
+// null
+
+let jogadores = null;
+
+console.log(jogadores);
+
+// undefined.
+
+let endereco = undefined;
+endereco = "Av Tomas De Sousa";
+
+console.log(endereco);
+
+// objeto literal.
+
+let adicionarnome = {
+    nome: "Wellington",
+    sobrenome: "Santos",
+    idade: 27,
+    altura: 1.77,
+    sexo: "M"
+}
+
+console.log(adicionarnome);
+
+// arrays
+
+let estados = ["RJ", "SP", "MG", "AM"];
+
+console.log(estados);
